802d9e40669941172a44ebd5473b850d6fbdc5b6
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-03-13 19:23:13 +0700

    chore(@/features/dashboard): add navlink active state

--- src/features/dashboard/components/layout/dashboard-navigation.tsx
+++ src/features/dashboard/components/layout/dashboard-navigation.tsx
@@ -1,7 +1,11 @@
+import { useRouterState } from "@tanstack/react-router";
 import { BaseNavigation } from "../../../../components";
 import { Box, Link, Stack, Typography } from "@mui/material";
 
 export function DashboardNavigation(): JSX.Element {
+  const { location } = useRouterState();
+  const pathName = location.pathname;
+
   return (
     <Box borderBottom="1px solid rgba(0, 0, 0, .1)">
       <BaseNavigation>
@@ -12,7 +16,7 @@ export function DashboardNavigation(): JSX.Element {
             sx={{
               cursor: "pointer",
               textDecorationLine: "none",
-              color: "black",
+              color: pathName.includes("products") ? "primary.main" : "black",
               "&:hover": {
                 color: "primary.main",
               },
@@ -21,14 +25,15 @@ export function DashboardNavigation(): JSX.Element {
           >
             All Products
           </Link>
-
           <Link
             href="/my-transactions"
             fontFamily="sans-serif"
             sx={{
               cursor: "pointer",
               textDecorationLine: "none",
-              color: "black",
+              color: pathName.includes("my-transactions")
+                ? "primary.main"
+                : "black",
               "&:hover": {
                 color: "primary.main",
               },
@@ -37,19 +42,19 @@ export function DashboardNavigation(): JSX.Element {
           >
             My Transactions
           </Link>
-
-          <Typography
-            bgcolor="primary.main"
-            color="white"
-            p={1}
-            borderRadius="100vw"
-            sx={{
-              cursor: "pointer",
-            }}
-          >
-            DC
-          </Typography>
         </Stack>
+
+        <Typography
+          bgcolor="primary.main"
+          color="white"
+          p={1}
+          borderRadius="100vw"
+          sx={{
+            cursor: "pointer",
+          }}
+        >
+          DC
+        </Typography>
       </BaseNavigation>
     </Box>
   );
