e9c253eb26bbbd400cc3ef23707d6ea4a39d9965
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-03-15 00:55:48 +0700

    chore(@/routes): add semi-middlewares

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,12 +1,22 @@
 import { BaseNavigation } from "../components";
 import { Container, Stack } from "@mui/material";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
+
+import Cookies from "js-cookie";
 
 export const Route = createFileRoute("/_auth")({
   component: AuthLayout,
 });
 
 function AuthLayout() {
+  const navigate = useNavigate();
+  // Should be located at beforeLoad as middleware, but currently there is issue
+  const token = Cookies.get("token");
+
+  if (token) {
+    navigate({ to: "/" });
+  }
+
   return (
     <Stack width="100wh" height="100vh">
       <BaseNavigation />
--- src/routes/_dashboard.tsx
+++ src/routes/_dashboard.tsx
@@ -4,14 +4,23 @@ import {
   DashboardNavigation,
 } from "../features/dashboard/components";
 import { Container, Stack } from "@mui/material";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-// import Cookies from "js-cookie";
+import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
+
+import Cookies from "js-cookie";
 
 export const Route = createFileRoute("/_dashboard")({
   component: DashboardLayout,
 });
 
 function DashboardLayout() {
+  const navigate = useNavigate();
+  // Should be located at beforeLoad as middleware, but currently there is issue
+  const token = Cookies.get("token");
+
+  if (!token) {
+    navigate({ to: "/login" });
+  }
+
   return (
     <Stack width="100wh" height="100vh">
       <DashboardNavigation />
